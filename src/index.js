573cc714dbb81d9d0dd5e7f0757650c755f841aa
adnanmulalic <adnan.mulalic3@gmail.com>
2023-02-15 12:45:12 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import "./style.css";
+import {Todos} from "./todos.js";
 
+let a = new Todos("a", "b", "c", "d");
 
+function createTodo(a, b, c, d) {
+    return new Todos(a, b, c, d);
+}
 
 /* function returnSomething() {
     alert("this works");
 }
 returnSomething(); */
 //export {returnSomething};
+
+function returnSomething() {
+    alert("this works");
+}
+//returnSomething();
+let todoItem = createTodo("a", "b", "c", "d");
+document.getElementById("main").append(todoItem);
+export {createTodo, returnSomething, todoItem};
