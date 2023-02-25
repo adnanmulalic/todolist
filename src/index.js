712c645f39848822f8b613726fbec54e7111e721
adnanmulalic <adnan.mulalic3@gmail.com>
2023-02-25 11:28:04 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -8,28 +8,50 @@ const submitButton = document.querySelector("#submitButton");
 const mainContent = document.querySelector("main");
 const title = document.querySelector("#title");
 const description = document.querySelector("#description");
-const urgent = document.querySelector("#urgent");
+const priority = document.querySelector("#priority");
 const dueDate = document.querySelector("#dueDate");
+const inputs = document.querySelectorAll(".inputs");
 let todoForm = document.querySelector("#todoForm");
+let todoDisplay = document.querySelector("#todoDisplay");
 // Code
 let todoProjects = [];
 let todoList = [];
+const isTrue = (currentTruth) => currentTruth === true; //MDN .every();
+
+function addTodo() {
+    let newTodo = new Todos(title.value, description.value, dueDate.value, priority.value);
+    todoList.push(newTodo);
+    let newTodoDiv = document.createElement("div");
+    displayTodo(newTodo).forEach(value => {
+        let currentValue = document.createElement("p");
+        currentValue.innerText = value;
+        newTodoDiv.appendChild(currentValue);
+        newTodoDiv.classList.add(".todo");
+    });
+    mainContent.appendChild(newTodoDiv);
+}
 createTodoBtn.addEventListener("click", () => {
     todoForm.classList.replace("form-hide", "form-display"); 
 })
 
 cancelButton.addEventListener("click", () => {
-    title.value = description.value = dueDate.value = ""; urgent.checked = false;
+    title.value = description.value = dueDate.value = "";
     todoForm.classList.replace("form-display", "form-hide");
     //submitButton.removeAttribute("disabled", "");
 })
 
 submitButton.addEventListener("click", () =>  {
-    let newTodo = new Todos(title.value, description.value, urgent.value, dueDate.value)
-    todoList.push(newTodo);
-    let newTodoDiv = document.createElement("div");
-    newTodoDiv.appendChild(displayTodo(newTodo));
-    mainContent.appendChild(newTodoDiv);
+    let arrayOfTruths = [];
+    inputs.forEach(input => {
+        arrayOfTruths.push(input.validity.valid)
+    });
+    if (arrayOfTruths.every(isTrue)) {
+        addTodo();
+        title.value = description.value = dueDate.value = "";
+        todoForm.classList.replace("form-display", "form-hide");
+    } else {
+        console.log("not working");
+    }
 })
 
 
